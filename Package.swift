c588b03c72c60fe51e1be3fc69f4f694dec88bca
sumpo <sumpo@azukid.com>
2026-04-22 20:32:18 +0900

    ローカライズ developmentRegion = en に変更（ja以外はenにフォールバックさせるため）

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 
 let package = Package(
     name: "AZDial",
-    defaultLocalization: "ja",
+    defaultLocalization: "en",
     platforms: [
         .iOS(.v16),
         .macOS(.v13),
